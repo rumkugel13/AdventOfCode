2812e74db375959c7d81cd0af92205e224c00726
rumkugel13 <rumkugel13@outlook.com>
2024-12-07 12:35:27 +0100

    speed up 2024/06

--- 2024-go/day06.go
+++ 2024-go/day06.go
@@ -18,26 +18,30 @@ func day06() {
 	input := ReadLines("input/day06.txt")
 	start := FindInGrid(input, '^')
 	dir := Point{-1, 0}
-	path := day06_get_path(input, start, dir)
+	length, path := day06_get_path(input, start, dir)
 
-	fmt.Println("Day 06 Part 01:", len(path))
+	fmt.Println("Day 06 Part 01:", length)
 
 	count := 0
-	for obstacle := range path {
-		if day06_is_loop(input, start, obstacle, dir) {
-			count++
+	visited := map[Point]bool{}
+	for i, obstacle := range path {
+		if _, v := visited[obstacle.point]; !v {
+			visited[obstacle.point] = true
+			if i > 0 && day06_is_loop(input, path[i-1].point, obstacle.point, obstacle.dir) {
+				count++
+			}
 		}
 	}
 
 	fmt.Println("Day 06 Part 02:", count)
 }
 
-func day06_is_loop(input []string, point, obstacle Point, dir Point) bool {
-	type visitor struct {
-		point Point
-		dir   Point
-	}
+type visitor struct {
+	point Point
+	dir   Point
+}
 
+func day06_is_loop(input []string, point, obstacle Point, dir Point) bool {
 	visited := map[visitor]bool{{point, dir}: false}
 	for {
 		next := point.Add(dir)
@@ -57,19 +61,21 @@ func day06_is_loop(input []string, point, obstacle Point, dir Point) bool {
 	return false
 }
 
-func day06_get_path(input []string, point Point, dir Point) map[Point]bool {
+func day06_get_path(input []string, point Point, dir Point) (int, []visitor) {
 	visited := map[Point]bool{point: false}
+	path := []visitor{}
 	for {
 		next := point.Add(dir)
 		if !InsideGrid(input, next) {
 			break
 		}
 		if input[next.row][next.col] != '#' {
+			path = append(path, visitor{next, dir})
 			point = next
 			visited[point] = true
 		} else {
 			dir.TurnRight()
 		}
 	}
-	return visited
+	return len(visited), path
 }
