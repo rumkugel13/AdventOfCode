6dff4bfa84fc137d28c11f34907c5156142b209a
rumkugel13 <rumkugel13@outlook.com>
2024-12-09 14:35:06 +0100

    2024/09: refactor

--- 2024-go/day09.go
+++ 2024-go/day09.go
@@ -23,11 +23,10 @@ func day09() {
 
 func day09_checksum(input []int) int {
 	sum := 0
-	for i := 0; i < len(input); i++ {
-		if input[i] == -1 {
-			continue
+	for i, val := range input {
+		if input[i] != -1 {
+			sum += val * i
 		}
-		sum += int(input[i]) * i
 	}
 	return sum
 }
@@ -35,42 +34,34 @@ func day09_checksum(input []int) int {
 func day09_compact2(input []int) []int {
 	for id := input[len(input)-1]; id > 0; id-- {
 		rstart := slices.Index(input, id)
-		rend := rstart
+		len_required := 0
 		for i := rstart; i < len(input) && input[i] == id; i++ {
-			rend = i
+			len_required++
 		}
-		len_required := rend - rstart + 1
 
 		lstart, lend := 0, 0
-		freespace := 0
-		for freespace < len_required && lend < len(input) {
-			off := lend
-			lstart = off + slices.Index(input[lend:], -1)
+		for (lend - lstart) < len_required {
+			lstart = lend + slices.Index(input[lend:], -1)
 			for i := lstart; i < len(input) && input[i] == -1; i++ {
 				lend = i
 			}
 			lend++
-			freespace = lend - lstart
 		}
 
-		if freespace < len_required || lstart > rstart {
+		if lstart > rstart {
 			continue
 		}
 
 		for i := 0; i < len_required; i++ {
 			input[i+lstart] = id
-		}
-		for i := rstart; i <= rend; i++ {
-			input[i] = -1
+			input[i+rstart] = -1
 		}
 	}
 	return input
 }
 
 func day09_compact(input []int) []int {
-	l := 0
-	r := len(input) - 1
-	for l < r {
+	for l, r := 0, len(input)-1; l < r; {
 		for l < r && input[l] != -1 {
 			l++
 		}
@@ -86,21 +77,13 @@ func day09_compact(input []int) []int {
 
 func day09_file_blocks(input string) []int {
 	blocks := []int{}
-	id := 0
-	for i := 0; i < len(input); i++ {
+	for i, id := 0, 0; i < len(input); i++ {
+		amount := int(input[i] - '0')
 		if (i & 1) == 0 {
-			files := make([]int, input[i]-'0')
-			for f := range len(files) {
-				files[f] = int(id)
-			}
-			blocks = append(blocks, files...)
+			blocks = append(blocks, slices.Repeat([]int{id}, amount)...)
 			id++
 		} else {
-			freespace := make([]int, input[i]-'0')
-			for f := range len(freespace) {
-				freespace[f] = -1
-			}
-			blocks = append(blocks, freespace...)
+			blocks = append(blocks, slices.Repeat([]int{-1}, amount)...)
 		}
 	}
 	return blocks
