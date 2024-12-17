e0ce6edab161634ce8e7e5931b0b03cb799ae35a
rumkugel13 <rumkugel13@outlook.com>
2024-12-17 14:25:33 +0100

    add 2024/16/2

--- 2024-go/README.md
+++ 2024-go/README.md
@@ -20,3 +20,4 @@ Solutions to [Advent of Code 2024](https://adventofcode.com/2024) in Go.
 | 13  | ✅      | ✅      |
 | 14  | ✅      | ✅      |
 | 15  | ✅      | ✅      |
+| 16  | ✅      | ✅      |
--- 2024-go/day16.go
+++ 2024-go/day16.go
@@ -45,51 +45,89 @@ func day16() {
 	input := ReadLines("input/day16.txt")
 	start := FindInGrid(input, 'S')
 	end := FindInGrid(input, 'E')
-	score := day16_score(input, start, end)
+	score, scores := day16_score(input, start, end)
 
 	fmt.Println("Day 16 Part 01:", score)
+
+	seats := day16_seats(scores, end, score)
+
+	fmt.Println("Day 16 Part 02:", seats)
 }
 
-func day16_score(grid []string, start, end Point) int {
-	type Visitor struct {
+func day16_seats(scores map[Visitor]int, end Point, minscore int) int {
+	type PointScore struct {
 		point Point
-		dir   Point
+		score int
 	}
 
-	visited := map[Point]int{}
+	seats := map[Point]bool{end: true}
+	queue := []PointScore{{end, minscore}}
+	for len(queue) > 0 {
+		pointScore := queue[0]
+		queue = queue[1:]
+
+		for _, dir := range Directions {
+			actualPoint := Visitor{pointScore.point, dir}
+
+			if score, ok := scores[actualPoint]; ok {
+				if score != pointScore.score {
+					continue
+				}
+
+				prev := pointScore.point.Add(OppositeDir(dir))
+				for _, dir2 := range Directions {
+					prevVis := Visitor{prev, dir2}
+
+					if val, found := scores[prevVis]; found {
+						if (val == score-1) || (val == score-1001) {
+							seats[prev] = true
+							queue = append(queue, PointScore{prev, val})
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return len(seats)
+}
+
+type Visitor struct {
+	point Point
+	dir   Point
+}
+
+func day16_score(grid []string, start, end Point) (int, map[Visitor]int) {
 	v := Visitor{start, Right}
+	visited := map[Visitor]int{v: 0}
 	queue := []Visitor{v}
-	visited[start] = 0
 	minScore := math.MaxInt64
 
 	for len(queue) > 0 {
 		visitor := queue[0]
 		queue = queue[1:]
-		score := visited[visitor.point]
+		score := visited[visitor]
 
 		if visitor.point == end {
 			if score < minScore {
 				minScore = score
 			}
+			continue
 		}
 
-		forward := visitor.point.Add(visitor.dir)
-		if v, ok := visited[forward]; GridChar(grid, forward) != '#' && (!ok || v > score+1) {
-			visited[forward] = score + 1
-			queue = append(queue, Visitor{forward, visitor.dir})
-		}
-
-		left := visitor.point.Add(visitor.dir.Left())
-		if v, ok := visited[left]; GridChar(grid, left) != '#' && (!ok || v > score+1001) {
-			visited[left] = score + 1 + 1000
-			queue = append(queue, Visitor{left, visitor.dir.Left()})
-		}
-
-		right := visitor.point.Add(visitor.dir.Right())
-		if v, ok := visited[right]; GridChar(grid, right) != '#' && (!ok || v > score+1001) {
-			visited[right] = score + 1 + 1000
-			queue = append(queue, Visitor{right, visitor.dir.Right()})
+		dirs := []Point{visitor.dir, visitor.dir.Left(), visitor.dir.Right(), OppositeDir(visitor.dir)}
+		newScores := []int{score + 1, score + 1001, score + 1001, score + 2001}
+		for i, dir := range dirs {
+			next := visitor.point.Add(dir)
+			if GridChar(grid, next) == '#' {
+				continue
+			}
+			nextVisitor := Visitor{next, dir}
+			if v, ok := visited[nextVisitor]; !ok || v > newScores[i] {
+				visited[nextVisitor] = newScores[i]
+				queue = append(queue, Visitor{next, dir})
+			}
 		}
 	}
-	return minScore
+	return minScore, visited
 }
